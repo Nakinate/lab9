de1a740cebbc72ffc734b610646764d0bb011b4b
Nakinate Buatip <nakinate1710@gmail.com>
2021-12-07 13:19:03 +0700

    complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,7 +3,19 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double s){
+	char g;
+	if (s > 90){
+		g = 'A';
+	}else if(s >75){
+		g = 'B';
+	}else if(s >60){
+		g = 'C';
+	}else if(s >45){
+		g = 'D';
+	}else g = 'F';	
+	return g;
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +29,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
